3d2a0c8adf5ba4337cadfefdc45ae2b77332c5ea
Lichosa <maria.martins.ramalho@escola.pr.gov.br>
2025-05-24 20:21:34 -0300

    Aula 7

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Filosofia',
+    'Quem é o autor da frase "penso logo existo"?',
+    'René Descartes'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o menor e o maior país do mundo?',
+    'O Vaiticano é o menor e a Rússia o maior país'
+)
+
+criaCartao(
+    'Matemática',
+    'Quantas casas decimais tem o número pi?',
+    'Pi tem infinitas casas decimais.'
+)
