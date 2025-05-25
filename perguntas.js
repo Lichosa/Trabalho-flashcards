6f5958f70f044a9bb4f17f29b5ed9e609f349469
Lichosa <maria.martins.ramalho@escola.pr.gov.br>
2025-05-24 21:35:34 -0300

    Trabalho finalizado

---
+++ perguntas.js
@@ -0,0 +1,71 @@
+criaCartao(
+    'Filosofia',
+    'Quem é o autor da frase "penso logo existo"?',
+    'René Descartes'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o menor e o maior país do mundo?',
+    'O Vaiticano é o menor e a Rússia o maior país'
+)
+
+criaCartao(
+    'Matemática',
+    'Quantas casas decimais tem o número pi?',
+    'Pi tem infinitas casas decimais.'
+)
+
+criaCartao(
+    'Artes',
+    'Quem pintou "Guernica"?',
+    'Pablo Picasso'
+)
+
+criaCartao(
+    'Química',
+    'Atualmente, quantos elementos químicos a tabela periódica possui?',
+    'Atualmente a tabela possui 118 elementos.'
+)
+
+criaCartao(
+    'Educação Fisíca',
+    'Qual o número mínimo de jogadores em cada time numa partida de futebol?',
+    'O mínimo é de sete jogadores.'
+)
+
+criaCartao(
+    'História',
+    'Qual a nacionalidade de Che Guevara?',
+    'Che Guevara era argentino.'
+)
+
+criaCartao(
+    'Ciências',
+    'Em que ordem surgiram os modelos atômicos?',
+    'Dalton, Thomson, Rutherford, Rutherford-Bohr.'
+)
+
+criaCartao(
+    'Fisíca',
+    'Qual a velocidade da luz?',
+    '299 792 458 metros por segundo (m/s).'
+)
+
+criaCartao(
+    'Filosofia',
+    'Qual foi o recurso utilizado inicialmente pelo homem para explicar a origem das coisas?',
+    ' A Mitologia.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual o maior animal terrestre?',
+    'Elefante africano.'
+)
+
+criaCartao(
+    'Geografia',
+    'Que líder mundial ficou conhecida como “Dama de Ferro”?',
+    'Margaret Thatcher.'
+)
